c944596233ba7204e920268b70ac7b18930266f8
unknown <poussigalitv@gmail.com>
2022-11-28 00:27:16 +1100

    some update

--- system/sys_app.py
+++ system/sys_app.py
@@ -1,5 +1,5 @@
 import system.app.maintenance.main as maintenance
-#import system.app.update.update_app as update
+import system.app.apt.install as update
 import system.app.config.config as config
 
-all_app={"maintenance":maintenance, "config":config} #, "app": update
+all_app={"maintenance":maintenance, "config":config, "app": update}
