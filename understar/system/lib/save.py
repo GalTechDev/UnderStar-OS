8a0f2aaf5945ea0e378bb1a01875151c609e0aed
GalTechDev <moreau.max.2004@gmail.com>
2025-01-23 12:01:39 +0100

    fix & rework

--- understar/system/lib/save.py
+++ understar/system/lib/save.py
@@ -9,7 +9,6 @@ class Save:
         self.path = None
         self.app_name = app_name
         self.save_path = os.path.join("save", "app")
-        self.app_save_path = os.path.join(f"{self.save_path}", f"{self.app_name}")
 
     def add_file(self, name: str, save_path: str = "", over_write: bool = False) -> None:
         """ajoute un fichier à sauvegarder"""
@@ -23,59 +22,66 @@ class Save:
             if not over_write:
                 raise FileExistsError
 
-            full_path = self.get_full_path(name, save_path)
-            os.remove(full_path)
-            self.add_file(name, save_path, over_write)
+            self.remove_file(name, save_path)
+            self.add_file(name, save_path, False)
 
-    def open(self, name: str, save_path: str = ""):
-        return self.factorize_save(name=name, save_path=save_path, mode="r")
+    def open(self, name: str, save_path: str = "", mode="r"):
+        """This function return a open file, dont forget to close it
+
+        Args:
+            name (str): _description_
+            save_path (str, optional): _description_. Defaults to "".
+            mode (str, optional): _description_. Defaults to "r".
+
+        Returns:
+            FileIO: a stream
+        """
+        save_path = self.get_full_path(name, save_path)
+        file = open(save_path, mode)
+        return file
 
     def read(self, name: str, save_path: str = "", binary_mode: bool = False):
-        return self.factorize_save(name=name, save_path=save_path, mode="r").read()
+        return self.factorize_save(name=name, save_path=save_path, mode="rb" if binary_mode else "b")
 
     def write(self, name, save_path: str = "", data: str = "", binary_mode: bool = False):
-        return self.factorize_save(name=name, save_path=save_path, mode='wb' if binary_mode else 'w', data=data)
+        return self.factorize_save(name=name, save_path=save_path, mode="wb" if binary_mode else "w", data=data)
 
     def json_read(self, name, save_path: str = "") -> dict:
         return json.loads(self.factorize_save(name=name, save_path=save_path, mode="r"))
 
     def get_files(self, save_path: str = ""):
-        return os.listdir(os.path.join(f"{self.app_save_path}", f"{save_path}"))
+        return os.listdir(self.get_full_path(save_path=save_path))
 
     def existe(self, name, save_path: str = ""):
         return name in self.get_files(save_path)
 
     def remove_file(self, name: str, save_path: str = ""):
         save_path = self.get_full_path(name, save_path)
-        os.remove(os.path.join(f"{save_path}", f"{self.app_name}", f"{name}"))
+        os.remove(save_path)
 
     def add_folder(self, save_path: str = "", ignore_exception: bool = True):
         try:
-            os.mkdir(os.path.join(f"{self.app_save_path}", f"{save_path}"))
+            os.mkdir(self.get_full_path(save_path=save_path))
 
         except Exception:
             if not ignore_exception:
-                raise Exception(f"Path: {save_path} can't be create")
+                raise Exception(f"Path: {save_path} can't be create for {self.app_name}")
 
     def remove_folder(self, save_path: str = ""):
-        rmtree(os.path.join(f"{self.app_save_path}", f"{save_path}"))
-        pass
+        rmtree(self.get_full_path(save_path=save_path))
 
     def get_tree(self, save_path: str = ""):
         tree: dict = {}
 
-        for folder in os.listdir(os.path.join(f"{self.app_save_path}", f"{save_path}")):
-            if save_path.isdir(os.path.join(f"{self.app_save_path}", f"{save_path}", f"{folder}")):
-                tree[folder] = self.get_tree(os.path.join(f"{save_path}", f"{folder}"))
+        for folder in os.listdir(self.get_full_path(save_path=save_path)):
+            if save_path.isdir(self.get_full_path(save_path=os.path.join(save_path, folder))):
+                tree[folder] = self.get_tree(os.path.join(save_path, folder))
 
         return tree
 
     def factorize_save(self, name: str, save_path: str = "", mode: str = "r", data: any = ""):
-        if not save_path:
-            save_path = os.path.join(f"{self.app_save_path}", f"{name}")
-
-        else:
-            save_path = os.path.join(f"{self.app_save_path}", f"{save_path}", f"{name}")
+        
+        save_path = self.get_full_path(name, save_path)
 
         encoding = "utf8" if "b" not in mode else None
 
@@ -87,10 +93,8 @@ class Save:
             with open(save_path, mode, encoding=encoding) as file:
                 return file.read()
 
-    def get_full_path(self, name: str , save_path: str = ""):
-        if not save_path:
-            save_path = os.path.join(f"{self.app_save_path}", f"{name}")
-        else:
-            save_path = os.path.join(f"{self.app_save_path}", f"{save_path}", f"{name}")
-
-        return save_path
+    def get_full_path(self, name: str = "", save_path: str = ""):
+        app_save_path = os.path.join(f"{self.save_path}", f"{self.app_name}")
+        full_path = os.path.join(app_save_path, save_path, name)
+        
+        return full_path
